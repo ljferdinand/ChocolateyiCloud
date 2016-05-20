12c7ccc6189482a9ab80361899b8c25a8db65348
Luke Ferdinand <ljferdinand@gmail.com>
2016-05-20 13:35:14 -0400

    Fixed issue with 32-bit installers

--- iCloud/tools/chocolateyinstall.ps1
+++ iCloud/tools/chocolateyinstall.ps1
@@ -1,11 +1,12 @@
 ﻿$packageName = 'iCloud'
 $version = '5.2'
 $url = 'http://download.info.apple.com/Mac_OS_X/031-31851-20160428-5490DFC6-0CC6-11E6-ADDA-3715400DF7EB/iCloudSetup.exe'
-#$url64bit = ''
+$url64bit = 'http://download.info.apple.com/Mac_OS_X/031-31851-20160428-5490DFC6-0CC6-11E6-ADDA-3715400DF7EB/iCloudSetup.exe'
 $fileType = 'msi'
 $silentArgs = '/qn /norestart'
 $validExitCodes = @(0,1603, 3010)
- 
+$x64 = Test-Path C:\windows\syswow64
+$rebootrequired 
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $downloadTempDir = Join-Path $toolsDir 'download-temp'
  
@@ -18,20 +19,35 @@ if ($app -and ([version]$app.Version -ge [version]$version)) {
     'No need to download and install again.'
   )
 } else {
+        Install-ChocolateyZipPackage -packageName $packageName -url $url `
+        -url64bit $url64bit -unzipLocation $downloadTempDir
  
-  Install-ChocolateyZipPackage -packageName $packageName -url $url `
-    -url64bit $url64bit -unzipLocation $downloadTempDir
- 
-  $msiFilesList = (Get-ChildItem -Path $downloadTempDir -Filter '*.msi' | Where-Object {
-    $_.Name -notmatch 'AppleSoftwareUpdate*.msi'
-  }).Name
- 
-  # Loop over each file and install it. iTunes requires all of them to be installed
-  foreach ($msiFileName in $msiFilesList) {
-    Install-ChocolateyInstallPackage -packageName $msiFileName -fileType $fileType `
-      -silentArgs $silentArgs -file (Join-Path $downloadTempDir $msiFileName) `
-      -validExitCodes $validExitCodes
-  }
- 
-  Remove-Item $downloadTempDir -Recurse
+        if ($x64 -eq "True") {
+            Write-Host "extracting 64-bit installers"
+            $msiFilesList = (Get-ChildItem -Path $downloadTempDir -Filter '*.msi' | Where-Object {
+                $_.Name -notmatch 'AppleSoftwareUpdate*.msi'
+            }).Name
+        }
+        else {
+            Write-Host "extracting 32-bit installers"
+            erase $downloadTempDir\*64.msi -force
+            $msiFilesList = (Get-ChildItem -Path $downloadTempDir -Filter '*.msi' | Where-Object {
+                $_.Name -notmatch 'AppleSoftwareUpdate*.msi'
+                }).Name
+        }
+
+        # Loop over each file and install it. iTunes requires all of them to be installed
+        foreach ($msiFileName in $msiFilesList) {
+        Install-ChocolateyInstallPackage -packageName $msiFileName -fileType $fileType `
+        -silentArgs $silentArgs -file (Join-Path $downloadTempDir $msiFileName) `
+        -validExitCodes $validExitCodes
+        }
+
+    Remove-Item $downloadTempDir -Recurse
+
+    if ($rebootrequired) {
+        Write-Host "Reboot Required"
+        exit 3010
+    }
+    
 }
